64fccc8f9c2a0897c4a25480b56ea995fa4f22d0
tnqphuong2003 <tnqphuong2003@gmail.com>
2022-10-04 15:03:53 +0700

    Code-Along: Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+    let color;
+    color = (count > 0) ? "green" : (count < 0) ? "red" : "black";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Counter</h1>
+      <span style={{color}} id="value">{count}</span>
+      <div className="btn-group">
+        <button onClick={() => setCount(count - 1)} className="btn btn-decrease">DECREASE</button>
+        <button onClick={() => setCount(0)} className="btn btn-reset">RESET</button>
+        <button onClick={() => setCount(count + 1)} className="btn btn-increase">INCREASE</button>
+      </div>
     </div>
   );
 }
